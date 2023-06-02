0718b07fbf141736c6880927e1f954f534b2c995
Jonathan VanDuyne <jonathanvanduyne@gmail.com>
2023-06-02 10:31:57 -0400

    new components

--- src/components/views/EmployeeViews.js
+++ src/components/views/EmployeeViews.js
@@ -5,6 +5,7 @@ import { ProductForm } from "../Products/ProductForm.js"
 import { EmployeeList } from "../Employees/EmployeeList.js"
 import { EmployeeForm } from "../Employees/EmployeeForm.js"
 import { CustomerList } from "../Customers/CustomerList.js"
+import { CustomerDetails } from "../Customers/CustomerDetails.js"
 
 export const EmployeeViews = () => {
 	return (
@@ -29,6 +30,8 @@ export const EmployeeViews = () => {
 			<Route path="employees/NewEmployeeForm" element={<EmployeeForm />} />
 			
 			<Route path="customers" element={<CustomerList />} />
+			
+			<Route path="customers/:customerId" element={<CustomerDetails />} />
 
 
 
